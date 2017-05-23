b9769a1ba3c8100c4e7929d046e84b46a544507c
151250082 <151250082@qq.com>
2017-05-23 10:28:48 +0800

    重新设置模板 orange 7 m 很无奈，，，

--- include/proc.h
+++ include/proc.h
@@ -39,8 +39,6 @@ typedef struct s_proc {
 
 	u32 pid;                   /* process id passed in from MM */
 	char p_name[16];           /* name of the process */
-
-	int nr_tty;
 }PROCESS;
 
 typedef struct s_task {
@@ -50,9 +48,8 @@ typedef struct s_task {
 }TASK;
 
 
-/* Number of tasks & procs */
-#define NR_TASKS	1
-#define NR_PROCS	3
+/* Number of tasks */
+#define NR_TASKS	4
 
 /* stacks of tasks */
 #define STACK_SIZE_TTY		0x8000
--- kernel/main.c
+++ kernel/main.c
@@ -28,23 +28,7 @@ PUBLIC int kernel_main()
 	char*		p_task_stack	= task_stack + STACK_SIZE_TOTAL;
 	u16		selector_ldt	= SELECTOR_LDT_FIRST;
 	int i;
-        u8              privilege;
-        u8              rpl;
-        int             eflags;
-	for (i = 0; i < NR_TASKS+NR_PROCS; i++) {
-                if (i < NR_TASKS) {     /* 任务 */
-                        p_task    = task_table + i;
-                        privilege = PRIVILEGE_TASK;
-                        rpl       = RPL_TASK;
-                        eflags    = 0x1202; /* IF=1, IOPL=1, bit 2 is always 1 */
-                }
-                else {                  /* 用户进程 */
-                        p_task    = user_proc_table + (i - NR_TASKS);
-                        privilege = PRIVILEGE_USER;
-                        rpl       = RPL_USER;
-                        eflags    = 0x202; /* IF=1, bit 2 is always 1 */
-                }
-
+	for (i = 0; i < NR_TASKS; i++) {
 		strcpy(p_proc->p_name, p_task->name);	// name of the process
 		p_proc->pid = i;			// pid
 
@@ -52,22 +36,26 @@ PUBLIC int kernel_main()
 
 		memcpy(&p_proc->ldts[0], &gdt[SELECTOR_KERNEL_CS >> 3],
 		       sizeof(DESCRIPTOR));
-		p_proc->ldts[0].attr1 = DA_C | privilege << 5;
+		p_proc->ldts[0].attr1 = DA_C | PRIVILEGE_TASK << 5;
 		memcpy(&p_proc->ldts[1], &gdt[SELECTOR_KERNEL_DS >> 3],
 		       sizeof(DESCRIPTOR));
-		p_proc->ldts[1].attr1 = DA_DRW | privilege << 5;
-		p_proc->regs.cs	= (0 & SA_RPL_MASK & SA_TI_MASK) | SA_TIL | rpl;
-		p_proc->regs.ds	= (8 & SA_RPL_MASK & SA_TI_MASK) | SA_TIL | rpl;
-		p_proc->regs.es	= (8 & SA_RPL_MASK & SA_TI_MASK) | SA_TIL | rpl;
-		p_proc->regs.fs	= (8 & SA_RPL_MASK & SA_TI_MASK) | SA_TIL | rpl;
-		p_proc->regs.ss	= (8 & SA_RPL_MASK & SA_TI_MASK) | SA_TIL | rpl;
-		p_proc->regs.gs	= (SELECTOR_KERNEL_GS & SA_RPL_MASK) | rpl;
+		p_proc->ldts[1].attr1 = DA_DRW | PRIVILEGE_TASK << 5;
+		p_proc->regs.cs	= ((8 * 0) & SA_RPL_MASK & SA_TI_MASK)
+			| SA_TIL | RPL_TASK;
+		p_proc->regs.ds	= ((8 * 1) & SA_RPL_MASK & SA_TI_MASK)
+			| SA_TIL | RPL_TASK;
+		p_proc->regs.es	= ((8 * 1) & SA_RPL_MASK & SA_TI_MASK)
+			| SA_TIL | RPL_TASK;
+		p_proc->regs.fs	= ((8 * 1) & SA_RPL_MASK & SA_TI_MASK)
+			| SA_TIL | RPL_TASK;
+		p_proc->regs.ss	= ((8 * 1) & SA_RPL_MASK & SA_TI_MASK)
+			| SA_TIL | RPL_TASK;
+		p_proc->regs.gs	= (SELECTOR_KERNEL_GS & SA_RPL_MASK)
+			| RPL_TASK;
 
 		p_proc->regs.eip = (u32)p_task->initial_eip;
 		p_proc->regs.esp = (u32)p_task_stack;
-		p_proc->regs.eflags = eflags;
-
-		p_proc->nr_tty = 0;
+		p_proc->regs.eflags = 0x1202; /* IF=1, IOPL=1 */
 
 		p_task_stack -= p_task->stacksize;
 		p_proc++;
@@ -77,12 +65,7 @@ PUBLIC int kernel_main()
 
 	proc_table[0].ticks = proc_table[0].priority = 15;
 	proc_table[1].ticks = proc_table[1].priority =  5;
-	proc_table[2].ticks = proc_table[2].priority =  5;
-	proc_table[3].ticks = proc_table[3].priority =  5;
-
-        proc_table[1].nr_tty = 0;
-        proc_table[2].nr_tty = 1;
-        proc_table[3].nr_tty = 1;
+	proc_table[2].ticks = proc_table[2].priority =  3;
 
 	k_reenter = 0;
 	ticks = 0;
@@ -104,8 +87,8 @@ void TestA()
 {
 	int i = 0;
 	while (1) {
-		printf("<Ticks:%x>", get_ticks());
-		milli_delay(200);
+		/* disp_str("A."); */
+		milli_delay(10);
 	}
 }
 
@@ -116,8 +99,8 @@ void TestB()
 {
 	int i = 0x1000;
 	while(1){
-		printf("B");
-		milli_delay(200);
+		/* disp_str("B."); */
+		milli_delay(10);
 	}
 }
 
@@ -128,7 +111,7 @@ void TestC()
 {
 	int i = 0x2000;
 	while(1){
-		printf("C");
-		milli_delay(200);
+		/* disp_str("C."); */
+		milli_delay(10);
 	}
 }
